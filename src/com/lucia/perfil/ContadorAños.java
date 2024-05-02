3b347485968b637e36ac19d0d5255851bf7cb6a1
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-02 21:29:40 +0200

    Creado metodo sumarAño()

--- src/com/lucia/perfil/ContadorAños.java
+++ src/com/lucia/perfil/ContadorAños.java
@@ -2,5 +2,14 @@
 package com.lucia.perfil;
 
 public class ContadorAños {
+    
+    /**
+     * Metodo que suma 1 año a los que tiene el pefil de la partida
+     * @param años del perfil
+     * @return años 
+     */
+    public static int sumarAño(int años){
+        return años+1;  
+    }
       
 }
