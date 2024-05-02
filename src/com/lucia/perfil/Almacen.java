2f41344514b49eec7049ecece58391081d3ee3db
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-02 20:03:02 +0200

    Creada clase Almacen: constructor + getters y setters

---
+++ src/com/lucia/perfil/Almacen.java
@@ -0,0 +1,41 @@
+
+package com.lucia.perfil;
+
+import java.util.HashMap;
+
+import com.lucia.producto.Producto;
+
+public class Almacen {
+    
+    private int espacioAlmacen;
+    
+    private HashMap<String,Producto> procuctosAlmacenados;
+    
+    // CONSTRUCTOR
+
+    public Almacen() {
+        this.espacioAlmacen = 50;
+        this.procuctosAlmacenados = new HashMap<>();
+    }
+    
+    // GETTERS
+
+    public int getEspacioAlmacen() {
+        return espacioAlmacen;
+    }
+
+    public HashMap<String, Producto> getProcuctosAlmacenados() {
+        return procuctosAlmacenados;
+    }
+    
+    // SETTERS
+
+    public void setEspacioAlmacen(int espacioAlmacen) {
+        this.espacioAlmacen = espacioAlmacen;
+    }
+
+    public void setProcuctosAlmacenados(HashMap<String, Producto> procuctosAlmacenados) {
+        this.procuctosAlmacenados = procuctosAlmacenados;
+    }
+
+}
