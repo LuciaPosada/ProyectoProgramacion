4b86c2f096cc1b6c2a5fac21d7acc10802916479
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-02 15:54:32 +0200

    Getters y Setters

--- src/com/lucia/producto/Producto.java
+++ src/com/lucia/producto/Producto.java
@@ -23,4 +23,48 @@ public class Producto {
         this.cantidad = cantidad;
     }
 
+    // GETTER
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public int getPrecioMin() {
+        return precioMin;
+    }
+
+    public int getPrecioMax() {
+        return precioMax;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    
+    // SETTER
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setPrecioMax(int precioMax) {
+        this.precioMax = precioMax;
+    }
+
+    public void setPrecioMin(int precioMin) {
+        this.precioMin = precioMin;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
 }
