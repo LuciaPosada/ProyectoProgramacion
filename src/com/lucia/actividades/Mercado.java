63307d281427f6acb3df508d9fba807d20cf5829
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-03 19:37:32 +0200

    Añadido metodod cambiarPrecios()

--- src/com/lucia/actividades/Mercado.java
+++ src/com/lucia/actividades/Mercado.java
@@ -1,7 +1,7 @@
 package com.lucia.actividades;
 
 import java.util.HashMap;
-
+import java.util.Random;
 
 import com.lucia.producto.Producto;
 
@@ -15,4 +15,16 @@ public class Mercado {
         this.procuctosEnVenta = new HashMap<>();
     }
 
+    /**
+     * Genera un nuevo precio aleatorio dentro del rango [precioMin, precioMax]
+     * @param precio El precio actual
+     * @param precioMax El precio máximo permitido
+     * @param precioMin El precio mínimo permitido
+     * @return El nuevo precio
+     */
+    public static int cambiarPrecios(int precio,int precioMax,int precioMin) {
+        Random random = new Random();
+        int precioNuevo = random.nextInt(precioMax - precioMin + 1) + precioMin;
+        return precioNuevo;
+    }
 }
