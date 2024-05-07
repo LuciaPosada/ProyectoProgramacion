de187043438221b7d911eb4f3ebcd3dd573ee6b9
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-07 21:06:58 +0200

    Añadido panel para botones, separador

--- src/com/lucia/interfaz/PanelPerfil.java
+++ src/com/lucia/interfaz/PanelPerfil.java
@@ -2,8 +2,10 @@
 package com.lucia.interfaz;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 
 public class PanelPerfil extends JPanel {
 
@@ -28,13 +30,30 @@ public class PanelPerfil extends JPanel {
 
             JLabel saludEtiqueta = new JLabel("[img] Salud: [num]");
             JLabel espacioEtiqueta = new JLabel("[img] Espacio: [num]");
-            JLabel placeholderEtiqueta = new JLabel("[img] Placeholder: [num]");
+            JLabel placeholderEtiqueta = new JLabel("[img] Placeholder: [num]"); // Placeholder
 
             panelStats.add(saludEtiqueta);
             panelStats.add(espacioEtiqueta);
             panelStats.add(placeholderEtiqueta);
 
 	// Panel Botones
+
+	    JPanel panelBtns = new JPanel(); // Pendiente de accion
+            panelBtns.setLayout(new BoxLayout(panelBtns, BoxLayout.X_AXIS));
+
+	    JButton btnVolver = new JButton("Volver al Inicio");
+
+	    JButton btnAvanzar = new JButton("Avanzar Año"); // Pendiente de accion
+
+	    panelBtns.add(btnVolver);
+	    panelBtns.add(btnAvanzar);
+
+        // Añadir elementos al PanelPerfil
+
+        add(panelInfo);
+        add(new JSeparator(JSeparator.HORIZONTAL));
+        add(panelStats);
+        add(panelBtns);
 
     }
 
