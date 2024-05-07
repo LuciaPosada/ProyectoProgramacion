119f37b7cf56325d322993c9882d07908a73ba8f
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-07 17:54:01 +0200

    Creado metodo comprobarDineroDisponible()

--- src/com/lucia/actividades/CompraVenta.java
+++ src/com/lucia/actividades/CompraVenta.java
@@ -3,6 +3,16 @@ package com.lucia.actividades;
 public class CompraVenta {
 
     /**
+     * Metodo que comprueba si el jugador tiene suficientes fondos para realizar la compra
+     * @param precioTotal  el precio total de la compra a realizar
+     * @param fondosPerfil los fondos disponibles en el perfil del jugador
+     * @return true si el usuario tiene suficientes fondos para realizar la compra, false en caso contrario
+     */
+    public static boolean comprobarDineroDisponible(int precioTotal,int fondosPerfil){
+        return precioTotal<= fondosPerfil;
+    }
+
+    /**
      * Calcula el precio total basado en la cantidad a comprar y el precio por unidad
      * @param cantidadComprar cantidad del producto a comprar
      * @param precioUnidad precio del producto a comprar
