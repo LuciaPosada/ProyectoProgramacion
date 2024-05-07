3aef8399122f463b4f3348db06447e22323e727c
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-07 18:01:32 +0200

    Creado metodo comprobarEspacioDisponible()

--- src/com/lucia/actividades/CompraVenta.java
+++ src/com/lucia/actividades/CompraVenta.java
@@ -3,6 +3,16 @@ package com.lucia.actividades;
 public class CompraVenta {
 
     /**
+     * Comprueba si hay suficiente espacio disponible en el almacén para comprar la cantidad requerida de productos
+     * @param espcioAlmacen La cantidad total de espacio disponible en el almacén
+     * @param cantidadComprar La cantidad de productos que se desea comprar
+     * @return true si hay suficiente espacio | false si no hay suficiente espacio
+     */
+    public static boolean comprobarEspacioDisponible(int espcioAlmacen,int cantidadComprar){
+        return cantidadComprar<= espcioAlmacen;
+    }
+
+    /**
      * Metodo que comprueba si el jugador tiene suficientes fondos para realizar la compra
      * @param precioTotal  el precio total de la compra a realizar
      * @param fondosPerfil los fondos disponibles en el perfil del jugador
