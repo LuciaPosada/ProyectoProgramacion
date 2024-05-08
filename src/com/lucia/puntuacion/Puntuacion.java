8786a05fa0925ef2916dff2fd4e920cf97d04681
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-08 12:11:02 +0200

    Añadido constructor

--- src/com/lucia/puntuacion/Puntuacion.java
+++ src/com/lucia/puntuacion/Puntuacion.java
@@ -6,5 +6,10 @@ public class Puntuacion {
     public int puntuacion;
     
     public String nombre;
+
+    public Puntuacion(int puntuacion, String nombre) {
+        this.puntuacion = puntuacion;
+        this.nombre = nombre;
+    }
     
 }
