44019e04e847ec80efb78e786400e611b5ba8dd5
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-09 21:37:26 +0200

    Creada clase PanelProducto

---
+++ src/com/lucia/interfaz/PanelProducto.java
@@ -0,0 +1,38 @@
+package com.lucia.interfaz;
+
+import java.awt.Color;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PanelProducto extends JPanel{
+
+    private JLabel nombreProductoLabel;
+    private JLabel precioActualLabel;
+    private JButton comprarBtn;
+    private JButton venderBtn;
+
+    public PanelProducto(/*producto*/){ // ToDo: Cambiar placeholders
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        setBackground(Color.PINK); // prueba
+
+        // Texto (Etiquetas)
+
+            nombreProductoLabel = new JLabel("[Producto]");
+            precioActualLabel = new JLabel("[Precio]");
+
+        // Botones
+        
+            comprarBtn = new JButton("Comprar");
+            venderBtn = new JButton("Vender");
+
+        // Añadir elementos al panel
+        
+        add(nombreProductoLabel);
+        add(precioActualLabel);
+        add(comprarBtn);
+        add(venderBtn);
+    }
+
+}
