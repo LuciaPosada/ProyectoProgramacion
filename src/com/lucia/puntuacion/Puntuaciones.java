3945d752605439572f56bd2bd48fb57be67828a3
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-10 10:23:38 +0200

    No se ha modificado nada,creo

--- src/com/lucia/puntuacion/Puntuaciones.java
+++ src/com/lucia/puntuacion/Puntuaciones.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class Puntuaciones {
     
     public static HashMap<String,Puntuacion> puntuaciones = new HashMap<>();
-    
+
     /**
      * Crea una puntuacion con los parametros especificados
      * @param nombre variable para el nuevo objecto
@@ -14,9 +14,18 @@ public class Puntuaciones {
      * @return la nueva puntuacion recien creada
      */
     public static Puntuacion crearNuevaPuntuacion(String nombre,int puntuacion){
-        Puntuacion puntuacionNuevo = new Puntuacion(puntuacion,nombre); 
+        Puntuacion puntuacionNuevo = new Puntuacion(puntuacion,nombre);
         puntuaciones.put("",puntuacionNuevo); // TODO: Metodo para generar claves en orden
-        return puntuacionNuevo; 
+        return puntuacionNuevo;
     }
-    
+
+    /**
+     * Obtiene la puntuación almacenada con la clave especificada.
+     * @param clave Clave de la puntuación a recuperar
+     * @return La puntuación asociada con la clave, o null si no existe.
+     */
+    public static Puntuacion getPuntuacion(String clave) {
+        return puntuaciones.get(clave);
+    }
+
 }
