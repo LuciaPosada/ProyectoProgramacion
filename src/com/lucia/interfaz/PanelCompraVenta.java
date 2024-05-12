c0f9aa89cdd185f7a289a8a126a562fd36a6859a
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-12 20:59:56 +0200

    Añadido slider y label

--- src/com/lucia/interfaz/PanelCompraVenta.java
+++ src/com/lucia/interfaz/PanelCompraVenta.java
@@ -15,12 +15,31 @@ public class PanelCompraVenta extends JDialog {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
+            // Panel Slider
+
+            JPanel sliderPanel = new JPanel(new BorderLayout());
+
+                // Slider
+
+                slider = new JSlider(JSlider.HORIZONTAL, 0, 100, 1); // ToDo: Cambiar maximo
+                slider.setMajorTickSpacing(20);
+                slider.setMinorTickSpacing(5);
+                slider.setPaintTicks(true);
+                slider.setPaintLabels(true);
+
+		sliderPanel.add(slider, BorderLayout.CENTER);
+
+                // JLabel
+
+                JLabel cantidadDinero = new JLabel("[precio]");
+
+	panel.add(sliderPanel);
            
         setContentPane(panel);
         setVisible(true);
     }
 
-    public static void main(String[] args) { // Preuba
+    public static void main(String[] args) { // Prueba
         SwingUtilities.invokeLater(() -> {
             new PanelCompraVenta();
         });
