ba4b1972f1feb06404c4f3ffced9d461e62329fa
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-12 17:01:45 +0200

    Retoques en la presentacion de los botones

--- src/com/lucia/interfaz/PanelInicio.java
+++ src/com/lucia/interfaz/PanelInicio.java
@@ -5,7 +5,6 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Box; 
-
 import java.awt.Color;
 
 public class PanelInicio extends JPanel {
@@ -22,11 +21,23 @@ public class PanelInicio extends JPanel {
         btnCargarPrt = new JButton("Cargar Partida");
         btnPuntuacion = new JButton("Puntuaciones");
 
+
+        // Estetica de los botones
+            Font buttonFont = new Font("Arial", Font.PLAIN, 17); 
+
+            btnNuevaPrt.setFont(buttonFont);
+            btnCargarPrt.setFont(buttonFont);
+            btnPuntuacion.setFont(buttonFont);
+
+            btnNuevaPrt.setPreferredSize(new java.awt.Dimension(150, 60));
+            btnCargarPrt.setPreferredSize(new java.awt.Dimension(150, 60));
+            btnPuntuacion.setPreferredSize(new java.awt.Dimension(150, 60));
+
         add(Box.createHorizontalGlue());
         add(btnNuevaPrt);
-        add(Box.createHorizontalStrut(10)); 
+        add(Box.createHorizontalStrut(25)); 
         add(btnCargarPrt);
-        add(Box.createHorizontalStrut(10)); 
+        add(Box.createHorizontalStrut(25)); 
         add(btnPuntuacion);
         add(Box.createHorizontalGlue());
     }
