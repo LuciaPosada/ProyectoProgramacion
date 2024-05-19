0698d637240a66c906dc9763a195304d1b5d4c74
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-19 21:55:03 +0200

    Empezado a planear las acciones que realizaran los botones

--- src/com/lucia/interfaz/PanelCiudad.java
+++ src/com/lucia/interfaz/PanelCiudad.java
@@ -3,6 +3,8 @@ package com.lucia.interfaz;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanelCiudad extends JPanel{
 
@@ -12,6 +14,13 @@ public class PanelCiudad extends JPanel{
         JButton hospital = new JButton("Ir al Hospital");
         JButton loteria = new JButton("Comprar un billete de loteria");
         JButton almacenes = new JButton("Aumentar espacio de almacen");
+
+        hospital.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /* Ciudad.comprarSalud(perfil); */ //ToDo: Añadir metodo + clase
+            }
+        });
 
         add(hospital);
         add(loteria);
