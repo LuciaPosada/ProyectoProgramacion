4f114054585d8ba94bf19df0ddf537ac5749d7de
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-19 18:04:20 +0200

    Añadido metodo setPerfil

--- src/com/lucia/interfaz/PanelJuego.java
+++ src/com/lucia/interfaz/PanelJuego.java
@@ -1,15 +1,18 @@
 package com.lucia.interfaz;
 
 import javax.swing.JPanel;
+import com.lucia.perfil.Perfil;
 import java.awt.BorderLayout;
 import java.awt.Color;
 
 public class PanelJuego extends JPanel {
 
+    private Perfil perfil;
+
     public PanelJuego() {
         setLayout(new BorderLayout());
 
-        PanelPerfil panelPerfil = new PanelPerfil();
+        PanelPerfil panelPerfil = new PanelPerfil(perfil);
         PanelTabs panelTabs = new PanelTabs();
 
         panelPerfil.setBackground(Color.BLUE);
@@ -17,5 +20,9 @@ public class PanelJuego extends JPanel {
 
         add(panelPerfil, BorderLayout.WEST);
         add(panelTabs, BorderLayout.CENTER);
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
     }
 }
