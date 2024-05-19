75fe251da97d14cb3775e7a958715f0e730b8e9b
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-19 18:24:26 +0200

    Creado metodo actualizarPanelPerfil()

--- src/com/lucia/interfaz/PanelJuego.java
+++ src/com/lucia/interfaz/PanelJuego.java
@@ -1,18 +1,21 @@
 package com.lucia.interfaz;
 
 import javax.swing.JPanel;
+
 import com.lucia.perfil.Perfil;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 
 public class PanelJuego extends JPanel {
 
     private Perfil perfil;
+    private PanelPerfil panelPerfil;
 
     public PanelJuego() {
         setLayout(new BorderLayout());
 
-        PanelPerfil panelPerfil = new PanelPerfil(perfil);
+        panelPerfil = new PanelPerfil(perfil);
         PanelTabs panelTabs = new PanelTabs();
 
         panelPerfil.setBackground(Color.BLUE);
@@ -24,5 +27,15 @@ public class PanelJuego extends JPanel {
 
     public void setPerfil(Perfil perfil) {
         this.perfil = perfil;
+    }
+
+    private void actualizarPanelPerfil() {
+        if (panelPerfil != null) {
+            remove(panelPerfil);
+        }
+        panelPerfil = new PanelPerfil(perfil);
+        add(panelPerfil);
+        revalidate();
+        repaint();
     }
 }
