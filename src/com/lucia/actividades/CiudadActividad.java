0cf943995b575afd9b903061da1922ed2e360203
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-20 11:25:23 +0200

    Creado metodo aumentarEspacio()

--- src/com/lucia/actividades/CiudadActividad.java
+++ src/com/lucia/actividades/CiudadActividad.java
@@ -4,9 +4,13 @@ public class CiudadActividad {
 
     private static int costoSalud = 100; // Costo Base
 
+    private static int costoEspacio = 50; // Costo Base
+
     public final static float MULTIPLICADOR_COSTO = 0.2f;
 
     public final static int CANTIDAD_AUMENTAR_SALUD = 20;
+
+    public final static int CANTIDAD_AUMENTAR_ESPACIO = 25;
 
     /**
      * Aumenta la salud del jugador en 20
@@ -19,11 +23,21 @@ public class CiudadActividad {
     }
 
     /**
+     * Aumenta la cantidad de espacio de almacenaje del jugador en 25
+     * @param espacioActual del perfil
+     * @return la nueva cantidad de espacio del almacen
+     */
+    public static int aumentarEspacio(int espacioActual) {
+        costoEspacio = aumentarCosto();
+        return espacioActual+CANTIDAD_AUMENTAR_ESPACIO;
+    }
+
+    /**
      * Aumenta el costo financiero
      * @return el nuevo costo
      */
     private static int aumentarCosto() {
         return (int) (costoSalud*MULTIPLICADOR_COSTO);
     }
+}
 
-} 
