96baa4df85b20d78157252b700480700c3062c62
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-20 12:01:40 +0200

    Añadido test para el metodo aumentarEspacio()

--- test/CiudadActividadesTest.java
+++ test/CiudadActividadesTest.java
@@ -1,7 +1,5 @@
 
 import com.lucia.actividades.CiudadActividad;
-import com.lucia.actividades.CompraVenta;
-import com.lucia.producto.Producto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +10,11 @@ public class CiudadActividadesTest {
     @Test
     public void comprobacionIrHospital() {
         Assertions.assertEquals(90, CiudadActividad.irHospital(70));
+    }
+
+    @Test
+    public void comprobacionAumentarEspacio() {
+        Assertions.assertEquals(75, CiudadActividad.aumentarEspacio(50));
     }
 
 }
