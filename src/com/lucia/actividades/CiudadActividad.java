60b19996943cdae4219a10e88fb0f2831f0f351d
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-20 11:35:22 +0200

    Creado metodo calcularPremioLoteria()

--- src/com/lucia/actividades/CiudadActividad.java
+++ src/com/lucia/actividades/CiudadActividad.java
@@ -1,16 +1,24 @@
 package com.lucia.actividades;
 
+import java.util.Random;
+
 public class CiudadActividad {
 
     private static int costoSalud = 100; // Costo Base
 
     private static int costoEspacio = 50; // Costo Base
 
+    private static int costoLoteria = 500; // Costo Base
+
     public final static float MULTIPLICADOR_COSTO = 0.2f;
 
     public final static int CANTIDAD_AUMENTAR_SALUD = 20;
 
     public final static int CANTIDAD_AUMENTAR_ESPACIO = 25;
+
+    public final static int MIN_GANANCIAS_LOTERIA = 50;
+
+    public final static int MAX_GANANCIAS_LOTERIA = 1000000;
 
     /**
      * Aumenta la salud del jugador en 20
@@ -33,6 +41,16 @@ public class CiudadActividad {
     }
 
     /**
+     * Genera la cantidad gana al jugar la loteria
+     * @return el dinero ganado
+     */
+    public static int calcularPremioLoteria() { //ToDo: Buscar manera de reducir las posibilidades de generar cantidades altas
+        costoLoteria = aumentarCosto();
+        Random random = new Random();
+        int premio = random.nextInt(MAX_GANANCIAS_LOTERIA - MIN_GANANCIAS_LOTERIA + 1) + MIN_GANANCIAS_LOTERIA;
+        return premio;
+    }
+    /**
      * Aumenta el costo financiero
      * @return el nuevo costo
      */
@@ -40,4 +58,3 @@ public class CiudadActividad {
         return (int) (costoSalud*MULTIPLICADOR_COSTO);
     }
 }
-
