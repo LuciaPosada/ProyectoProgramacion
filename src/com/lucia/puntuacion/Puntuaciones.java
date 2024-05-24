854e39b802c5c4e4bdc84eebe4f39af33e7a5b1e
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-24 11:17:47 +0200

    Creado metodo crearPuntuacion

--- src/com/lucia/puntuacion/Puntuaciones.java
+++ src/com/lucia/puntuacion/Puntuaciones.java
@@ -1,9 +1,38 @@
 
 package com.lucia.puntuacion;
 
-import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Puntuaciones {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/proyecto";
+
+    private static final String USER = "postgres";
+
+    private static final String PASSWORD = "postgres";
     
+    public static void crearPuntuacion(String nombre,int puntuacion) {
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            String query = "INSERT INTO public.puntuaciones (nompart,puntuacion) VALUES (?, ?)";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, nombre);
+            statement.setInt(2, puntuacion);
+
+            int rowsInserted = statement.executeUpdate();
+
+            if (rowsInserted > 0) {
+                System.out.println("INSERT 1");
+            } else {
+                System.out.println("INSERT 0");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al añadir fila: " + e.getMessage());
+        }
+    }
 
 }
