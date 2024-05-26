0437b10f5e530a695d9448592cb2b1220ed51748
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-26 20:07:01 +0200

    Cambiado placeholder para el precio (solo 1 unidad de producto)

--- src/com/lucia/interfaz/DialogCompraVenta.java
+++ src/com/lucia/interfaz/DialogCompraVenta.java
@@ -1,9 +1,9 @@
 package com.lucia.interfaz;
 
+import com.lucia.actividades.CompraVenta;
 import com.lucia.producto.Producto;
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -13,61 +13,62 @@ public class DialogCompraVenta extends JDialog {
     private JButton aceptarBtn;
     private JSlider slider;
     private JTextField cantidadSeleccionadaTxt;
+    private JLabel cantidadDinero;
 
-    public DialogCompraVenta(JFrame parent,Producto producto) {
+    public DialogCompraVenta(JFrame parent, Producto producto) {
         super(parent, true);
         setTitle("Comprar/Vender"); // Pendiente de revision
-        setSize(400, 200); 
+        setSize(400, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-            // Panel Slider
+        // Panel Slider
 
-            JPanel sliderPanel = new JPanel(new BorderLayout());
+        JPanel sliderPanel = new JPanel(new BorderLayout());
 
-                // Slider
+        // Slider
 
-                slider = new JSlider(JSlider.HORIZONTAL, 1, producto.getCantidad(), 1); // ToDo: Arreglar que el maximo no se vea
-                slider.setMajorTickSpacing(20);
-                slider.setMinorTickSpacing(5);
-                slider.setPaintTicks(true);
-                slider.setPaintLabels(true);
+        slider = new JSlider(JSlider.HORIZONTAL, 1, producto.getCantidad(), 1); // ToDo: Arreglar que el maximo no se vea
+        slider.setMajorTickSpacing(20);
+        slider.setMinorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
 
-                // Numero del Slider - JTextField
+        // Numero del Slider - JTextField
 
-                cantidadSeleccionadaTxt = new JTextField(5);
-                cantidadSeleccionadaTxt.setEditable(false);
-                cantidadSeleccionadaTxt.setHorizontalAlignment(JTextField.CENTER);
+        cantidadSeleccionadaTxt = new JTextField(5);
+        cantidadSeleccionadaTxt.setEditable(false);
+        cantidadSeleccionadaTxt.setHorizontalAlignment(JTextField.CENTER);
 
-                slider.addChangeListener(new ChangeListener() {
-                    public void stateChanged(ChangeEvent e) {
-                        actualizarCantidadSeleccionadaTxt(slider.getValue());
-                    }
-                });
+        slider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                actualizarCantidadSeleccionadaTxt(slider.getValue());
+            }
+        });
 
-                // JLabel
+        // JLabel
 
-                JLabel cantidadDinero = new JLabel("[precio] €");
+        cantidadDinero = new JLabel(CompraVenta.calcularPrecioTotal(1, producto.getPrecio()) + " €");
 
-            JPanel informacionPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-            informacionPanel.add(cantidadSeleccionadaTxt);
-            informacionPanel.add(cantidadDinero);
+        JPanel informacionPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        informacionPanel.add(cantidadSeleccionadaTxt);
+        informacionPanel.add(cantidadDinero);
 
-            sliderPanel.add(slider, BorderLayout.CENTER);
-            sliderPanel.add(informacionPanel, BorderLayout.SOUTH);
+        sliderPanel.add(slider, BorderLayout.CENTER);
+        sliderPanel.add(informacionPanel, BorderLayout.SOUTH);
 
-            // Panel Botones
+        // Panel Botones
 
-            JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
-            cancelarBtn = new JButton("Cancelar");
-            aceptarBtn = new JButton("Aceptar");
+        cancelarBtn = new JButton("Cancelar");
+        aceptarBtn = new JButton("Aceptar");
 
-            btnPanel.add(cancelarBtn);
-            btnPanel.add(aceptarBtn);
+        btnPanel.add(cancelarBtn);
+        btnPanel.add(aceptarBtn);
 
         panel.add(sliderPanel);
         panel.add(btnPanel);
@@ -75,8 +76,8 @@ public class DialogCompraVenta extends JDialog {
         setContentPane(panel);
         setVisible(true);
     }
-    
-    public void actualizarCantidadSeleccionadaTxt(int cantidad){
+
+    private void actualizarCantidadSeleccionadaTxt(int cantidad) {
         cantidadSeleccionadaTxt.setText(Integer.toString(cantidad));
     }
 }
