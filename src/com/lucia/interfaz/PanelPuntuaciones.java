6a7073af3d9e09d44031a4290863cfe86148bcc9
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-30 23:29:10 +0200

    Modificado panel para que muestra paneles Producto

--- src/com/lucia/interfaz/PanelPuntuaciones.java
+++ src/com/lucia/interfaz/PanelPuntuaciones.java
@@ -1,8 +1,11 @@
 package com.lucia.interfaz;
 
-import com.lucia.producto.Producto;
+import java.util.ArrayList;
 
 import javax.swing.*;
+
+import com.lucia.puntuacion.Puntuacion;
+import com.lucia.puntuacion.Puntuaciones;
 
 public class PanelPuntuaciones extends JScrollPane {
 
@@ -10,8 +13,16 @@ public class PanelPuntuaciones extends JScrollPane {
 
     public PanelPuntuaciones() {
         panelContenedor = new JPanel();
-        panelContenedor.setLayout(new BoxLayout(panelContenedor, BoxLayout.X_AXIS));
+        panelContenedor.setLayout(new BoxLayout(panelContenedor, BoxLayout.Y_AXIS));
 
+        ArrayList<Puntuacion> puntuaciones = Puntuaciones.obtenerTodasLasPuntuaciones();
+
+        for (Puntuacion puntuacion : puntuaciones) { // Crea paneles para las puntuaciones
+            PanelPuntuacion panelPuntuacion = new PanelPuntuacion(puntuacion);
+            panelContenedor.add(panelPuntuacion);
+        }
+
+        this.setViewportView(panelContenedor);
     }
 
 }
