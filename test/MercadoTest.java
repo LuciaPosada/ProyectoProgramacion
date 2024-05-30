9d8307dab95910a3c108ca9aa066560afd5ed8b3
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-30 22:13:49 +0200

    Creado test para metodo renovar cantidades

--- test/MercadoTest.java
+++ test/MercadoTest.java
@@ -4,4 +4,16 @@ import org.junit.jupiter.api.Test;
 
 public class MercadoTest {
 
+    @Test
+    public void comprobacionRenovarCantidades() {
+        int cantidadMax = 40;
+        int cantidadMin = 15;
+
+        for (int i = 0; i < 1000; i++) { // Repeticion
+            int resultado = Mercado.renovarCantidades(cantidadMax, cantidadMin);
+            Assertions.assertTrue("El resultado debe estar dentro del rango", resultado >= cantidadMin && resultado <= cantidadMax);
+        }
+    }
+
+
 }
