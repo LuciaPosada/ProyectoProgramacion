b087f51c62d5ef777ce1e5f338968f7149116739
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-30 23:24:49 +0200

    Cambioado fuente de puntuacionLabel

--- src/com/lucia/interfaz/PanelPuntuacion.java
+++ src/com/lucia/interfaz/PanelPuntuacion.java
@@ -3,15 +3,20 @@ package com.lucia.interfaz;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Font;
 import com.lucia.puntuacion.Puntuacion;
 
 public class PanelPuntuacion extends JPanel{
+
+    Font fuente = new Font("Arial", Font.PLAIN, 16);
 
     public PanelPuntuacion(Puntuacion puntuacion){
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         JLabel nombreLabel = new JLabel(puntuacion.nombre);
         JLabel puntuacionLabel = new JLabel(String.valueOf(puntuacion.puntuacion));
+        
+        puntuacionLabel.setFont(fuente);
 
         add(nombreLabel);
         add(puntuacionLabel);
