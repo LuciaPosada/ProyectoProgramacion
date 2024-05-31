5dc98b3a0f4a92290433f3aec560e1d43eac6415
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-05-31 18:38:59 +0200

    Añadido metodos al listener del boton hospital

--- src/com/lucia/interfaz/PanelCiudad.java
+++ src/com/lucia/interfaz/PanelCiudad.java
@@ -2,13 +2,23 @@ package com.lucia.interfaz;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import com.lucia.actividades.CiudadActividad;
+import com.lucia.actividades.CompraVenta;
+import com.lucia.perfil.Perfil;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PanelCiudad extends JPanel{
 
-    public PanelCiudad() {
+    private Perfil perfil;
+    private PanelPerfil panelPerfil;
+
+    public PanelCiudad(Perfil perfil,PanelPerfil panelPerfil) {
+        this.perfil = perfil;
+        this.panelPerfil = panelPerfil;
         setLayout(new GridLayout(3,1));
 
         JButton hospital = new JButton("Ir al Hospital");
@@ -18,7 +28,12 @@ public class PanelCiudad extends JPanel{
         hospital.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /* Ciudad.comprarSalud(perfil); */ //ToDo: Añadir metodo + clase
+                if(CompraVenta.comprobarDineroDisponible(CiudadActividad.getCostoSalud(),perfil.getFondos())){
+                    int nuevaSalud = CiudadActividad.irHospital(perfil.getSalud());
+                    perfil.setSalud(nuevaSalud);
+                    perfil.setFondos(CompraVenta.gastarDinero(perfil.getFondos(), CiudadActividad.getCostoSalud()));
+                    panelPerfil.actualizarInformacion(); 
+                }
             }
         });
 
