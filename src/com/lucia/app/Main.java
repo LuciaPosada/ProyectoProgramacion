0a99d64844b3431c5af08f8cc751303c88209993
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-06-01 20:18:34 +0200

    Creado Main

---
+++ src/com/lucia/app/Main.java
@@ -0,0 +1,16 @@
+
+package com.lucia.app;
+
+import com.lucia.interfaz.VentanaPrincipal;
+
+/**
+ * @author Lucia Posada
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+    
+}
