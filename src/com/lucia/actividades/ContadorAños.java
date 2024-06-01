44e8acf8ddc88f062849df4216078ae865db85df
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-06-01 20:26:37 +0200

    Clase cambiada de package

---
+++ src/com/lucia/actividades/ContadorAños.java
@@ -0,0 +1,41 @@
+
+package com.lucia.actividades;
+
+public class ContadorAños {
+    
+    /**
+     * Metodo que suma 1 año a los que tiene el pefil de la partida
+     * @param años del perfil
+     * @return años 
+     */
+    public static int sumarAño(int años){
+        return años+1;  
+    }
+    
+    /**
+     * Metodo que reduce la salud en base a los años del perfil
+     * @param salud del perfil
+     * @param años del perfil
+     * @return salud con la cantidad correspondiente reducida
+     */
+    public static int empeorarSalud(int salud,int años){
+        
+        final int SALUD_A_EMPEORAR_ENTRE_20_40_AÑOS = 10;
+        final int SALUD_A_EMPEORAR_ENTRE_40_60_AÑOS = 20;
+        final int SALUD_A_EMPEORAR_MAYOR_DE_60_AÑOS = 30;
+        
+        final int VEINTE_AÑOS = 20;
+        final int CUARENTA_AÑOS = 40;
+        final int SESENTA_AÑOS = 60;
+        
+        if(años>VEINTE_AÑOS&&años<CUARENTA_AÑOS){
+            return salud - SALUD_A_EMPEORAR_ENTRE_20_40_AÑOS;
+        }else if (años>=CUARENTA_AÑOS&&años<=SESENTA_AÑOS){
+            return salud - SALUD_A_EMPEORAR_ENTRE_40_60_AÑOS;
+        }else if(años>SESENTA_AÑOS){
+            return salud - SALUD_A_EMPEORAR_MAYOR_DE_60_AÑOS;
+        }else{ // menores de 20
+            return salud;
+        }  
+    }   
+}
