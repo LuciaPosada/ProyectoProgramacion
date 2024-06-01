e9a82fffdfc679061d469618a8cf9c026b9447a4
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-06-01 21:13:09 +0200

    Refactorizado

--- src/com/lucia/interfaz/PanelMercado.java
+++ src/com/lucia/interfaz/PanelMercado.java
@@ -3,21 +3,19 @@ package com.lucia.interfaz;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
-import com.lucia.actividades.Mercado;
+import com.lucia.perfil.Mercado;
 import com.lucia.producto.Producto;
 
 public class PanelMercado extends JScrollPane {
 
     JPanel panelContenedor;
-
     Mercado mercado = new Mercado();
 
     public PanelMercado() {
         panelContenedor = new JPanel();
         panelContenedor.setLayout(new BoxLayout(panelContenedor, BoxLayout.Y_AXIS));
 
-        for (Producto producto : mercado.getProductosEnVenta().values()) { // Crea paneles para los productos
+        for (Producto producto : mercado.getProductosEnVenta().values()) {
             PanelProducto panelProducto = new PanelProducto(producto);
             panelProducto.setAlignmentX(LEFT_ALIGNMENT);
             panelContenedor.add(panelProducto);
