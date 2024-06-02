7e5b0cd23ccd80c2f5e9449b53e1ace39353db7f
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-06-02 15:33:32 +0200

    Añadido nuevas variables para la BD

--- src/com/lucia/puntuacion/Puntuacion.java
+++ src/com/lucia/puntuacion/Puntuacion.java
@@ -7,9 +7,18 @@ public class Puntuacion {
     
     public String nombre;
 
-    public Puntuacion(int puntuacion, String nombre) {
+    public int ganancias;
+
+    public int perdidas;
+
+    public int años;
+
+    public Puntuacion(int puntuacion, String nombre, int ganancias, int perdidas, int años) {
         this.puntuacion = puntuacion;
         this.nombre = nombre;
+        this.ganancias = ganancias;
+        this.perdidas = perdidas;
+        this.años = años;
     }
     
 }
